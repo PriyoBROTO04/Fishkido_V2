5bd573b83d7b4ffe16d2c18b00fc49d1a995bb84
Priyobroto Kar <priyobrotokar@gmail.com>
2023-04-21 14:38:28 +0530

    Added Projects Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
         'Soft-pink':'hsla(330, 34%, 58%, 0.4)',
         'brown':'hsla(0, 14%, 66%, 1)',
         'pink':'hsla(300, 20%, 67%, 1)',
+        'White':'hsla(0, 0%, 72%, 0.35)',
         'pastel':'#FFD8C8',
         'orange':'#B39488',
         'Off-white':'#D9D9D9',
