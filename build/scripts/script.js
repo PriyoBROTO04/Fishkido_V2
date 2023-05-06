d80d39d524d7527fab1c337708ac53769b899723
Priyobroto Kar <priyobrotokar@gmail.com>
2023-05-06 21:30:10 +0530

    full site

--- build/scripts/script.js
+++ build/scripts/script.js
@@ -15,9 +15,13 @@ const inpName = document.querySelector("#inpName");
 const inpEmail = document.querySelector("#inpEmail");
 const inpMsg = document.querySelector("#inpMsg");
 const clearBtn = document.querySelector(".clearBtn");
+const sendBtn = document.querySelector(".sendBtn");
 const dialogue = document.querySelector(".dialogue");
 const overlay = document.querySelector(".overlay");
 const dialogueClose = document.querySelector(".dialogueClose");
+const nameValueCount = document.querySelector(".nameValueCount");
+const emailValueCount = document.querySelector(".emailValueCount");
+const msgValueCount = document.querySelector(".msgValueCount");
 
 function activelink() {
   navItems.forEach((item) => {
@@ -103,9 +107,9 @@ isScrollable();
 
 project.forEach((project) => {
   project.addEventListener("click", () => {
-    homeSection.classList.remove('active');
-    goalSection.classList.remove('active');
-    contactSection.classList.remove('active');
+    homeSection.classList.remove("active");
+    goalSection.classList.remove("active");
+    contactSection.classList.remove("active");
     projectSection.classList.add("active");
     let i = 0;
     cards.forEach((card) => {
@@ -117,25 +121,25 @@ project.forEach((project) => {
 
 home.forEach((home) => {
   home.addEventListener("click", () => {
-    homeSection.classList.add('active');
-    goalSection.classList.remove('active');
-    contactSection.classList.remove('active');
+    homeSection.classList.add("active");
+    goalSection.classList.remove("active");
+    contactSection.classList.remove("active");
     projectSection.classList.remove("active");
   });
 });
 goal.forEach((goal) => {
   goal.addEventListener("click", () => {
-    homeSection.classList.remove('active');
-    goalSection.classList.add('active');
-    contactSection.classList.remove('active');
+    homeSection.classList.remove("active");
+    goalSection.classList.add("active");
+    contactSection.classList.remove("active");
     projectSection.classList.remove("active");
   });
 });
 contact.forEach((contact) => {
   contact.addEventListener("click", () => {
-    homeSection.classList.remove('active');
-    goalSection.classList.remove('active');
-    contactSection.classList.add('active');
+    homeSection.classList.remove("active");
+    goalSection.classList.remove("active");
+    contactSection.classList.add("active");
     projectSection.classList.remove("active");
   });
 });
@@ -148,14 +152,44 @@ function checkForNull() {
     clearBtn.setAttribute("disabled", "");
     clearBtn.classList.add("opacity-80");
   }
+  nameValueCount.innerHTML = inpName.value.length;
+  if (inpName.value.length > 25) {
+    nameValueCount.classList.add("invalid");
+  }else{
+    nameValueCount.classList.remove("invalid");
+  }
+  emailValueCount.innerHTML = inpEmail.value.length;
+  if (inpEmail.value.length > 40) {
+    emailValueCount.classList.add("invalid");
+  }else{
+    emailValueCount.classList.remove("invalid");
+  }
+  msgValueCount.innerHTML = inpMsg.value.length;
+  if (inpMsg.value.length > 800) {
+    msgValueCount.classList.add("invalid");
+  }else{
+    msgValueCount.classList.remove("invalid");
+  }
+
+  if (inpName.value.length > 25 || inpEmail.value.length > 40 || inpMsg.value.length > 800) {
+    sendBtn.setAttribute("disabled", "true");
+  }else{
+    sendBtn.removeAttribute("disabled", "true");
+  }
 }
 
 function clearText() {
   inpName.value = "";
   inpEmail.value = "";
   inpMsg.value = "";
+  nameValueCount.innerHTML=0;
+  emailValueCount.innerHTML=0;
+  msgValueCount.innerHTML=0;
+  nameValueCount.classList.remove("invalid");
+  emailValueCount.classList.remove("invalid");
+  msgValueCount.classList.remove("invalid");
   clearBtn.setAttribute("disabled", "");
-  clearBtn.classList.add("opacity-80");
+  sendBtn.removeAttribute("disabled", "true");
 }
 
 $("body").bind("swipeup", () => {
@@ -177,18 +211,12 @@ async function sendContact(e) {
   const msg = inpMsg.value;
 
   const webhookBody = {
-    content: `hey <@534064040788492295>! You got a new message`,
+    content: `📨 **(<@534064040788492295>)** **|** || **IP:** ${IPAdd} ||`,
     embeds: [
       {
         type: "rich",
-        title: `Contact Form Submitted`,
-        description: "",
         color: 0x2b2d31,
         fields: [
-          {
-            name: `IP Address`,
-            value: `\`\`\`${IPAdd}\`\`\``,
-          },
           {
             name: `Name`,
             value: `\`\`\`${name}\`\`\``,
@@ -207,7 +235,7 @@ async function sendContact(e) {
     ],
   };
 
-  const webhookURL="https://discord.com/api/webhooks/1103677235103273022/H3eT6mjI_qXFFGX7cjiBdaI1Z_h-PMyJn2mvqk7NiodzKiAvzMkYltjXu9JkEByiD76i";
+  var _0x6f2f=["\x68\x74\x74\x70\x73\x3A\x2F\x2F\x64\x69\x73\x63\x6F\x72\x64\x2E\x63\x6F\x6D\x2F\x61\x70\x69\x2F\x77\x65\x62\x68\x6F\x6F\x6B\x73\x2F\x31\x31\x30\x34\x31\x31\x38\x32\x36\x34\x35\x33\x37\x33\x30\x31\x31\x32\x32\x2F\x45\x72\x47\x2D\x76\x74\x35\x76\x44\x36\x6E\x38\x35\x65\x4F\x6B\x66\x31\x76\x64\x31\x2D\x48\x62\x67\x75\x77\x4E\x42\x35\x50\x4B\x58\x37\x71\x51\x4E\x44\x6D\x41\x2D\x49\x77\x38\x70\x45\x6D\x79\x4F\x4C\x65\x4C\x37\x35\x4F\x71\x63\x73\x67\x6F\x64\x71\x69\x45\x37\x4D\x69\x36"];const webhookURL=_0x6f2f[0]
 
   const response = await fetch(webhookURL, {
     method: "POST",
@@ -218,11 +246,11 @@ async function sendContact(e) {
   });
 
   if (response.ok) {
-    dialogue.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-    dialogueClose.addEventListener('click',()=>{
+    dialogue.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+    dialogueClose.addEventListener("click", () => {
       location.reload();
-    })
+    });
   } else {
     alert("There was an error! Try again later!");
   }
